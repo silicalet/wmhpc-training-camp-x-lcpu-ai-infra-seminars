91d291d7eea8aee31b5be84f0dd48175ba5ad753
Mr. why <silicalet@outlook.com>
2026-07-31 17:02:28 +0800

    impl m2/01 and add clangd support

--- assignment01/cuda/m2_first_kernel/01_vector_add.cu
+++ assignment01/cuda/m2_first_kernel/01_vector_add.cu
@@ -2,13 +2,22 @@
 // 六个空各考一个概念，填完编译运行，"PASS"即可。
 // 填完之前这个文件无法通过编译。
 #include "common.h"
+// #include <__clang_cuda_builtin_vars.h>
+// #include <__clang_cuda_runtime_wrapper.h>
+#include <driver_types.h>
 
 // ====== 空 1：kernel 需要什么函数修饰符？ ======
-/* 填这里 */ void vectorAdd(const float *a, const float *b, float *c, int n) {
+/* 填这里 */
+__global__ void vectorAdd(const float *a, const float *b, float *c, int n) {
     // ====== 空 2：这个线程负责的全局下标 ======
-    int idx = /* 填这里 */;
+    int idx = blockIdx.x * blockDim.x + threadIdx.x
+    /* 填这里 */
+    ;
     // ====== 空 3：边界保护——总线程数可能多于元素个数 ======
-    if (/* 填这里 */) {
+    if (
+        /* 填这里 */
+        idx < n
+    ) {
         c[idx] = a[idx] + b[idx];
     }
 }
@@ -31,15 +40,23 @@ int main() {
     CUDA_CHECK(cudaMalloc(&d_c, bytes));
 
     // ====== 空 4：把 h_a、h_b 拷到 device（注意最后一个方向参数） ======
-    CUDA_CHECK(cudaMemcpy(d_a, h_a, bytes, /* 填这里 */));
-    CUDA_CHECK(cudaMemcpy(d_b, h_b, bytes, /* 填这里 */));
+    CUDA_CHECK(cudaMemcpy(d_a, h_a, bytes,
+        /* 填这里 */
+        cudaMemcpyHostToDevice
+    ));
+    CUDA_CHECK(cudaMemcpy(d_b, h_b, bytes,
+        /* 填这里 */
+        cudaMemcpyHostToDevice
+    ));
 
     int threadsPerBlock = 256;
     // ====== 空 5：block 数——向上取整，保证覆盖全部 n 个元素 ======
-    int blocksPerGrid = /* 填这里 */;
+    int blocksPerGrid = /* 填这里 */
+        (n + threadsPerBlock - 1) / threadsPerBlock;
 
     // ====== 空 6：启动 kernel（执行配置写在哪里？） ======
-    vectorAdd /* 填这里 */ (d_a, d_b, d_c, n);
+    vectorAdd /* 填这里 */
+       <<<blocksPerGrid, threadsPerBlock>>> (d_a, d_b, d_c, n);
     CUDA_CHECK_KERNEL();
 
     CUDA_CHECK(cudaMemcpy(h_c, d_c, bytes, cudaMemcpyDeviceToHost));
